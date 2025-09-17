976170dc17b24213d998372990003c27e12aab88
Bwk66666618 <bwesley66618@gmail.com>
2025-09-17 08:29:23 -0400

    Created a character, and tis fast, and can fly

---
+++ UnityEssentialsThing/Assets/_Unity Essentials/Scripts/Colectible.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Colectible : MonoBehaviour
+{
+	public float rotationSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+	
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	rotationSpeed =+ rotationSpeed + 0.01f;
+        transform.Rotate(0, rotationSpeed, 0);
+    }
+}
---
+++ UnityEssentialsThing/Assets/_Unity Essentials/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+// Controls player movement and rotation.
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 40.0f; // Set player's movement speed.
+    public float rotationSpeed = 359.0f; // Set player's rotation speed.
+
+    private Rigidbody rb; // Reference to player's Rigidbody.
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>(); // Access player's Rigidbody.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Handle physics-based movement and rotation.
+    private void FixedUpdate()
+    {
+        // Move player based on vertical input.
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = transform.forward * moveVertical * 20 * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement);
+
+	if (Input.GetKey(KeyCode.Space))
+	{
+        Vector3 moement = transform.up * 10 * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + moement);
+	}
+        // Rotate player based on horizontal input.
+        float turn = Input.GetAxis("Horizontal") * 10 * rotationSpeed * Time.fixedDeltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        rb.MoveRotation(rb.rotation * turnRotation);
+    }
+}
